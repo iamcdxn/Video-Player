4f9ac65552cb0fd255ecefc500a010613c0b4744
jamesKe <iamcdxn@gmail.com>
2017-09-06 14:25:15 +0800

    Add SwiftLint

---
+++ VideoPlayer/VideoPlayer/VideoPlayerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by CdxN on 2017/9/6.
+//  Copyright © 2017年 CdxN.io. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
